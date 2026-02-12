0b3c31b3a4ca43d68b9a3b6622e8a473e98fe660
visrust <madharchodambani@gmail.com>
2026-02-12 14:40:27 +0530

    removed toggleterm shading

--- lua/user/config/ide/ide/toggleterm.lua
+++ lua/user/config/ide/ide/toggleterm.lua
@@ -10,8 +10,8 @@ require('toggleterm').setup({
 
     open_mapping = [[<c-\>]], -- universal toggle
     hide_numbers = true,
-    shade_terminals = true,
-    shading_factor = 2,
+    -- shade_terminals = true,
+    -- shading_factor = 2,
     start_in_insert = true,
     insert_mappings = true,
     terminal_mappings = true,
